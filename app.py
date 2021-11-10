2df75a5b7626dd893955807705d734117d6d8945
Dhruv Gangwani <42980624+DhruvGangwani@users.noreply.github.com>
2021-11-10 18:32:04 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ from credentials import consumer_key, consumer_secret, access_token, access_toke
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
-    
+import base64
+  
 
 qa_tokenizer = AutoTokenizer.from_pretrained("./models/qa_model")
 qa_model = AutoModelForQuestionAnswering.from_pretrained("./models/qa_model")
@@ -19,7 +20,7 @@ sent_tokenizer = AutoTokenizer.from_pretrained("./models/sentiment_model")
 sent_model = AutoModelForSequenceClassification.from_pretrained("./models/sentiment_model")
 sent_model = pipeline('text-classification', model=sent_model, tokenizer=sent_tokenizer)
 
-def compute(text, aspects):
+def compute(text, aspects, qa_model, sent_model):
 	#preprocessing
 	preprocess_text = preprocess(text)
 	#get nouns
@@ -27,7 +28,7 @@ def compute(text, aspects):
 	#get alternative names of aspects
 	aspect_classes = get_similar_words(noun_list, aspects)
 	#get sentiment
-	sentiment_result = get_sentiment(aspect_classes, text)
+	sentiment_result = get_sentiment(aspect_classes, text, qa_model, sent_model)
 	return sentiment_result
 
 
@@ -47,9 +48,10 @@ def aspect_sentiment(aspects, hashtag):
 	twitter_content = get_tweets(hashtag, consumer_key, consumer_secret, access_token, access_token_secret, tweet_count=50)
 	aspect_score = {asp : {'positive': 0, 'negative': 0} for asp in aspects}
 	
+	#compute scores for each tweet
 	if twitter_content.to_dict():
 		for text in twitter_content['text']:
-			sentiment_result = compute(text, aspects)
+			sentiment_result = compute(text, aspects, qa_model, sent_model)
 			for result in sentiment_result:
 				score = sentiment_result[result]
 				if score>0:
@@ -66,9 +68,21 @@ def aspect_sentiment(aspects, hashtag):
 	result_list = [[k, 'positive', v['positive']] for k,v in aspect_score.items()]
 	result_list.extend([[k, 'negative', v['negative']] for k,v in aspect_score.items()])
 
+	#plot the bar plot across all aspects
 	aspects_df = pd.DataFrame(result_list, columns= ['aspect', 'sentiment', 'score'])
 	sns.barplot(x = 'aspect', y = 'score', hue='sentiment', data=aspects_df)
-	plt.show()
+	plt.savefig('result.png')
+
+	#send base64 string of image as response
+	img_result = None
+	with open('result.png', 'rb') as f:
+		im_b64 = base64.b64encode(f.read())
+		img_result = str(im_b64)
+	if img_result:
+		return {'statusCode': 200, 'body': json.dumps(img_result)}
+	else:
+		return {'statusCode': 400, 'body': 'issue in saving result image'}
+
 
 
 # aspects = ['screen', 'battery', 'design', 'camera']
