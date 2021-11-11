c9ed31c8b49c0a294d64bbd2390a63e88a85b927
Dhruv Gangwani <42980624+DhruvGangwani@users.noreply.github.com>
2021-11-11 17:12:01 +0530

    Add files via upload

--- twitter/scrap_data.py
+++ twitter/scrap_data.py
@@ -8,15 +8,13 @@ def get_tweets(hashtag, consumer_key, consumer_secret, access_token, access_toke
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth,wait_on_rate_limit=True)
 
-    count=0
+    
     tweet_text, tweet_date = list(), list()
-    for tweet in tweepy.Cursor(api.search,q="#iphone13",count=1, lang="en").items():
+    for tweet in tweepy.Cursor(api.search,q=hashtag, count=tweet_count, lang="en").items():
         tweet_text.append(tweet.text.encode('utf-8'))
         tweet_date.append(tweet.created_at)
         # csvWriter.writerow([tweet.created_at, tweet.text.encode('utf-8')])
-        count+=1
-        if count>tweet_count:
-            break
+        
     content = pd.DataFrame({'text': tweet_text, 'date': tweet_date})
     return content
 
